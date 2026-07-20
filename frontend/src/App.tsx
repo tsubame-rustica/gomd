002f25c3f1db90f2b8e6764e6d3ba1e6e17c7050
tsubame-rustica <tsubame.bird.chorui@gmail.com>
2026-07-20 15:32:38 +0900

    MarkdownをHTMLで渡すAPI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,24 @@ import { useEffect, useState } from 'react'
 
 function App() {
   // Goから受け取るメッセージを入れるハコ（初期値は Loading...）
-  const [message, setMessage] = useState<string>("Loading...")
+  const [content, setContent] = useState<string>("Loading...")
 
   useEffect(() => {
     // Viteプロキシ経由で Go API にアクセス
-    fetch('/api/hello')
+    fetch('/api/memo/test/hoge')
       .then(res => res.json())
       .then(data => {
         // Goから送られてきた data.message をセット
-        setMessage(data.message)
+        setContent(data.message)
       })
       .catch(err => {
         console.error("Fetch error:", err)
-        setMessage("APIからの取得に失敗しました")
+        setContent("APIからの取得に失敗しました")
       })
   }, [])
 
   return (
-    <p className="text-xl font-medium">
-      {message}
-    </p>
+    <div className="prose prose-blue" dangerouslySetInnerHTML={{ __html: content }} />
   )
 }
 
